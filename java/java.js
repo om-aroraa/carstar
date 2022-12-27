6fce0ab2261e43c92ea8e0a942032d657e2b18a2
om-aroraa <120252758+om-aroraa@users.noreply.github.com>
2022-12-28 04:53:06 +0530

    Trying to fix js

--- java/java.js
+++ java/java.js
@@ -1,7 +1,9 @@
 function am(f, a){
-    let path = 'D:\\carstar\\all cars';
+    let path = 'https://om-aroraa.github.io/carstar//all%20cars';
     window.location.replace(`${path}\\${f}\\${a}.html`);
 }
+
+console.log("Script Loaded!")
 
 window.onscroll = () => {
     this.scrollY > 20 ? document.getElementById("navi").classList.add("sticky") : document.getElementById("navi").classList.remove("sticky");
