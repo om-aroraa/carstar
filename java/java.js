b5041d6252b82072fc21b52b8e25a75929119907
om-aroraa <120252758+om-aroraa@users.noreply.github.com>
2022-12-30 17:16:39 +0530

    fixing popularcars files and other js stuff

--- java/java.js
+++ java/java.js
@@ -8,3 +8,8 @@ console.log("Javascript Script Loaded!")
 window.onscroll = () => {
     this.scrollY > 20 ? document.getElementById("navi").classList.add("sticky") : document.getElementById("navi").classList.remove("sticky");
 }
+
+function ap(f,a){
+    let path = '/carstar/Popularcars';
+    window.location.href = `${path}/${f}/${a}.html`;
+}
